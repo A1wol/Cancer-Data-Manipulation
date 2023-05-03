457181529388720b1a7a0342fa94eb6aa9fc9869
A1wol <a1wol@protonmail.com>
2023-05-03 18:55:00 +0200

    Table data created

--- src/components/dataImport.vue
+++ src/components/dataImport.vue
@@ -1,19 +1,48 @@
 <template>
     <div class="data">
-        <div v-for="row in getDataRows()">
-            {{ row }}
-        </div>
     </div>
 </template>
 <script setup>
-import { onMounted } from "vue"
-import breastCancer from "@/dist/data/breast-cancer-wisconsin.data"
+import { onMounted, ref } from "vue"
+import breastCancerData from "@/dist/data/breast-cancer-wisconsin.data"
 
-function getDataRows() {
-    return breastCancer.split("\n")
+const tableData = ref([]);
+const itemsPerPage = ref(5);
+const tableHeaders = ref([
+    {
+        title: 'Dessert (100g serving)',
+        align: 'start',
+        sortable: false,
+        key: 'name',
+    },
+    { title: 'Calories', align: 'end', key: 'calories' },
+    { title: 'Fat (g)', align: 'end', key: 'fat' },
+    { title: 'Carbs (g)', align: 'end', key: 'carbs' },
+    { title: 'Protein (g)', align: 'end', key: 'protein' },
+    { title: 'Iron (%)', align: 'end', key: 'iron' },
+]);
+
+function getData() {
+    breastCancerData.split("\n").forEach(element => {
+        const elementValues = element.split(",")
+        tableData.value.push(
+            {
+                radius: elementValues[0],
+                texture: elementValues[1],
+                perimeter: elementValues[2],
+                area: elementValues[3],
+                smoothness: elementValues[4],
+                compactness: elementValues[5],
+                concavity: elementValues[6],
+                concavePoints: elementValues[7],
+                symmetry: elementValues[8],
+                fractalDimension: elementValues[9]
+            }
+        )
+    });
 }
 onMounted(() => {
-    getDataRows()
+    getData();
 })
 </script>
 <style lang="scss"></style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,15 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
-
 // Vuetify
 import { createVuetify } from 'vuetify'
+import { VDataTable } from 'vuetify/labs/VDataTable'
 
 export default createVuetify(
+  {
+    components: {
+      VDataTable,
+    },
+  }
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 )
