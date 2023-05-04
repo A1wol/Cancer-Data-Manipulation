91d84ea9c7a3a6e4339adaf257fa420268629de3
A1wol <a1wol@protonmail.com>
2023-05-04 02:23:15 +0200

    graph working

--- src/components/dataGraph.vue
+++ src/components/dataGraph.vue
@@ -4,15 +4,15 @@
             <GraphHeader />
         </div>
         <div class="graph__chart">
-            <apexchart width="600" type="bar" :options="options" :series="series"></apexchart>
+            <apexchart ref="realTimeChart" width="700" type="bar" :options="options" :series="series"></apexchart>
 
             <div class="graph__category-select">
                 <div class="text-h4">Choose category: </div>
                 <v-item-group class="d-flex flex-column" selected-class="bg-primary">
                     <div v-for="category in graphCategories" :key="category">
                         <v-item v-slot="{ selectedClass, toggle }">
-                            <v-card :class="['d-flex align-center', selectedClass]" @click="toggle">
-                                <div class="text-h5 text-center graph__category" @click="selectedCategory = category">
+                            <v-card :class="['d-flex align-center', selectedClass]" @click="selectedCategory = category">
+                                <div class="text-h5 text-center graph__category" @click="toggle">
                                     {{ category }}
                                 </div>
                             </v-card>
@@ -28,48 +28,63 @@ import { ref, onMounted, computed, watch } from "vue"
 import { useStore } from "vuex"
 import GraphHeader from "./graphHeader.vue";
 
+const realTimeChart = ref()
 const store = useStore()
 const graphCategories = ref([])
-const selectedCategory = ref('radius')
-const seriesData = ref([])
-const seriesCategories = ref([])
+const selectedCategory = ref()
 const tableItems = computed(() => store.getters.getTableItems);
 const categoryValues = computed(() => tableItems.value.map(el => el[selectedCategory.value]));
-const xAxisCategories = ref(...new Set([categoryValues.value]))
-const xAxisDuplicates = ref(xAxisCategories.value.map(value => [{ "value": value, "duplicateCount": categoryValues.value.filter(str => str === value).length }]));
+const xAxisDuplicates = computed(() => [...new Set(categoryValues.value)].map(value => new Object({ "value": value, "duplicateCount": categoryValues.value.filter(str => str === value).length })));
 
 function getSeriesData() {
-    let seriesValues = [],
-        seriesCategory = []
-    xAxisDuplicates.value.forEach(element => {
-        if (element[0].duplicateCount > 2) {
-            seriesValues.push(element[0].duplicateCount)
-            seriesCategory.push(element[0].value)
+    let arr1 = [], arr2 = []
+    xAxisDuplicates.value.forEach(el => {
+        if (el.duplicateCount > 1) {
+            if (el.value !== undefined) {
+                arr1.push(el.value)
+            }
+            else {
+                arr1.push('undefined')
+            }
+            arr2.push(el.duplicateCount)
         }
-    });
-    seriesData.value = seriesValues;
-    seriesCategories.value = seriesCategory;
+    })
+    series.value[0].data = arr2;
+    options.value.xaxis.categories = arr1;
 }
 function getGraphCategories() {
     graphCategories.value = Object.keys(tableItems.value[0]);
 }
 const options = ref({
     xaxis: {
-        // categories: seriesCategories._rawValue,
-        categories: ['a', 'b', 'c', 'd', 'a', 'b', 'c', 'd', 'a', 'b', 'c', 'd', 'a', 'b']
+        categories: [],
+        title: {
+            text: 'Values'
+        }
+    },
+    yaxis: {
+        title: {
+            text: 'Amount of duplicates'
+        }
     }
 })
 const series = ref([{
-    // data: seriesData._rawValue
-    data: [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+    data: []
 }])
 watch(selectedCategory, () => {
     getSeriesData();
-    console.log(seriesData._rawValue, [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2])
+    realTimeChart.value.updateSeries(([{
+        data: series.value[0].data
+    }], false, true))
+    realTimeChart.value.updateOptions({
+        xaxis: {
+            categories: options.value.xaxis.categories
+        }
+    })
 })
+
 onMounted(() => {
     getGraphCategories();
-    getSeriesData()
 })
 </script>
 <style scoped lang="scss">
