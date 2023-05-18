80fc39fb1dacac864cb0bd82c3467e81bb2bc9ca
A1wol <a1wol@protonmail.com>
2023-05-18 08:00:11 +0200

    text fields validation added

--- src/components/tableRowUpdate.vue
+++ src/components/tableRowUpdate.vue
@@ -1,30 +1,30 @@
 <template>
     <div class="row-update mt-5">
         <div class="row-update__column">
-            <v-text-field v-model="updatedRow.radius" density="compact" variant="outlined" prepend-icon="mdi-radius-outline"
-                label="Radius" hint="Enter proper radius" class="mb-2" />
-            <v-text-field v-model="updatedRow.texture" density="compact" variant="outlined" prepend-icon="mdi-texture-box"
-                label="Texture" hint="Enter proper texture" class="mb-2" />
-            <v-text-field v-model="updatedRow.perimeter" density="compact" variant="outlined" prepend-icon="mdi-dots-circle"
-                label="Perimeter" hint="Enter proper perimeter" class="mb-2" />
-            <v-text-field v-model="updatedRow.area" density="compact" variant="outlined" prepend-icon="mdi-chart-areaspline"
-                label="Area" hint="Enter proper area" class="mb-2" />
-            <v-text-field v-model="updatedRow.smoothness" density="compact" variant="outlined"
+            <v-text-field v-model="updatedRow.radius" type="number" density="compact" variant="outlined"
+                prepend-icon="mdi-radius-outline" label="Radius" hint="Enter proper radius" class="mb-2" />
+            <v-text-field v-model="updatedRow.texture" type="number" density="compact" variant="outlined"
+                prepend-icon="mdi-texture-box" label="Texture" hint="Enter proper texture" class="mb-2" />
+            <v-text-field v-model="updatedRow.perimeter" type="number" density="compact" variant="outlined"
+                prepend-icon="mdi-dots-circle" label="Perimeter" hint="Enter proper perimeter" class="mb-2" />
+            <v-text-field v-model="updatedRow.area" type="number" density="compact" variant="outlined"
+                prepend-icon="mdi-chart-areaspline" label="Area" hint="Enter proper area" class="mb-2" />
+            <v-text-field v-model="updatedRow.smoothness" type="number" density="compact" variant="outlined"
                 prepend-icon="mdi-wall-sconce-flat-variant-outline" class="mb-2" label="Smoothness"
                 hint="Enter proper smoothness" />
         </div>
         <div class="row-update__column">
-            <v-text-field v-model="updatedRow.compactness" density="compact" variant="outlined"
+            <v-text-field v-model="updatedRow.compactness" type="number" density="compact" variant="outlined"
                 prepend-icon="mdi-view-compact-outline" label="Compactness" hint="Enter proper compactness" class="mb-2" />
-            <v-text-field v-model="updatedRow.concavity" density="compact" variant="outlined"
+            <v-text-field v-model="updatedRow.concavity" type="number" density="compact" variant="outlined"
                 prepend-icon="mdi-wall-sconce-round-variant-outline" label="Concavity" hint="Enter proper concavity"
                 class="mb-2" />
-            <v-text-field v-model="updatedRow.concavePoints" density="compact" variant="outlined"
+            <v-text-field v-model="updatedRow.concavePoints" type="number" density="compact" variant="outlined"
                 prepend-icon="mdi-progress-star-four-points" label="Concave Points" hint="Enter proper concavePoints"
                 class="mb-2" />
-            <v-text-field v-model="updatedRow.symmetry" density="compact" variant="outlined"
+            <v-text-field v-model="updatedRow.symmetry" type="number" density="compact" variant="outlined"
                 prepend-icon="mdi-math-integral" label="Symmetry" hint="Enter proper symmetry" class="mb-2" />
-            <v-text-field v-model="updatedRow.fractalDimension" density="compact" variant="outlined"
+            <v-text-field v-model="updatedRow.fractalDimension" type="number" density="compact" variant="outlined"
                 prepend-icon="mdi-fraction-one-half" label="Fractal Dimension" hint="Enter proper fractal dimension"
                 class="mb-2" />
         </div>
